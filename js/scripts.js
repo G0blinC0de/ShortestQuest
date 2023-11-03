c322b6b8ca335aa49a908a5ab3309f7f08368e88
G0blinC0de <brewbrotherred@gmail.com>
2023-11-03 11:34:11 -0700

    fix class reference in final paragraph

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,23 @@
-window.onload = function() {
+document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('select-form');
     const classtype = document.getElementById('classtype');
     const warriorStory = document.getElementById('warriorStory');
     const wizardStory = document.getElementById('wizardStory');
     const rogueStory = document.getElementById('rogueStory');
+    
+    const successParagraph = document.getElementById('successParagraph');
+    const failureParagraph = document.getElementById('failureParagraph');
 
     form.addEventListener('submit', function(event) {
         event.preventDefault();
-
+        const generateButton = document.getElementById('generateButton');
         // Hide all story paragraphs
         warriorStory.classList.add('hidden');
         wizardStory.classList.add('hidden');
         rogueStory.classList.add('hidden');
 
         // Determine which Class was selected
-        const selectedOption = classtype.value;
+        const selectedOption = classtype.options[classtype.selectedIndex].value;
         if (selectedOption === 'Warrior') {
             warriorStory.classList.remove('hidden');
         } else if (selectedOption === 'Wizard') {
@@ -22,9 +25,15 @@ window.onload = function() {
         } else if (selectedOption === 'Rogue') {
             rogueStory.classList.remove('hidden');
         }
+
+        // Update the success and failure paragraphs with the selected class
+        document.getElementById('successClassSpan').textContent = selectedOption;
+        document.getElementById('failureClassSpan').textContent = selectedOption;
+        
     });
 
-    generateButton.addEventListener('click', function() {
+    generateButton.addEventListener('click', function(event) {
+        event.preventDefault();
         const randomNumber = Math.floor(Math.random() * 20) + 1;
 
         // Show the appropriate result paragraph based on the random number
@@ -36,8 +45,8 @@ window.onload = function() {
             failureParagraph.classList.remove('hidden');
         }
     });
-    classSelectedSpan.textContent = classtype.value;
-};
+});
+
 
 /* function reset() {
     document.getElementById
